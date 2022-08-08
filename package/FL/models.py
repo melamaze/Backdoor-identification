ad18e514c32a8ba058483df2e7f5928479740eab
melamela <p500297@gmail.com>
2022-08-09 01:19:51 +0800

    revise model.py

--- package/FL/models.py
+++ package/FL/models.py
@@ -6,33 +6,28 @@ class CNN_Model(nn.Module):
     def __init__(self):
         super(CNN_Model, self).__init__()
         # Convolution 1 , input_shape=(1,28,28)
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=0) #output_shape=(16,24,24)
-        self.relu1 = nn.ReLU() # activation
-        # Max pool 1
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2) #output_shape=(16,12,12)
-        # Convolution 2
-        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0) #output_shape=(32,8,8)
-        self.relu2 = nn.ReLU() # activation
-        # Max pool 2
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2) #output_shape=(32,4,4)
-        # Fully connected 1 ,#input_shape=(32*4*4)
-        self.fc1 = nn.Linear(32 * 4 * 4, 10) 
-    
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=0), #output_shape=(16,24,24)
+            nn.ReLU(), # activation
+            # Max pool 1
+            nn.MaxPool2d(kernel_size=2), #output_shape=(16,12,12)
+            # Convolution 2
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0),#output_shape=(32,8,8)
+            nn.ReLU(), # activation
+            # Max pool 2
+            nn.MaxPool2d(kernel_size=2) #output_shape=(32,4,4)
+        )
+        self.linear = nn.Sequential(
+            # Fully connected 1 ,#input_shape=(32*4*4)
+            nn.Linear(32 * 4 * 4, 32), 
+            nn.ReLU(),
+            nn.Linear(32, 10)
+        )
     def forward(self, x):
-        # Convolution 1
-        out = self.cnn1(x)
-        out = self.relu1(out)
-        # Max pool 1
-        out = self.maxpool1(out)
-        # Convolution 2 
-        out = self.cnn2(out)
-        out = self.relu2(out)
-        # Max pool 2 
-        out = self.maxpool2(out)
-        out = out.view(out.size(0), -1)
-        # Linear function (readout)
-        out = self.fc1(out)
-        return out
+        x = self.conv(x)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+        return x
 
 class Network(nn.Module):
     def __init__(self):
